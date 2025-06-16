a6743e01577d04bd5ed7ed21a7533fe06561f15d
David <dkwon22@gmail.com>
2025-06-15 23:10:37 -0400

    Expo bottom tabs

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,48 @@
+import { Tabs } from "expo-router";
+import { Feather } from "@expo/vector-icons";
+
+export default function TabsLayout() {
+    return (
+        // color of the icon when the tab is active
+        // labels are not shown by default
+    <Tabs screenOptions={{ tabBarActiveTintColor: "#BCC247", tabBarShowLabel: false}}> 
+        // title for each Tabs - how they're displayed in the tab bar
+        <Tabs.Screen
+            name="index" // name of the screen
+            options={{
+                title: "Home", // shown titleof the tab
+                tabBarIcon: ({size,color}) => (
+                    <Feather name="home" size={size} color={color} /> 
+                ),
+            }}
+        />
+        <Tabs.Screen
+            name="search" 
+            options={{
+                title: "Search", 
+                tabBarIcon: ({size,color}) => (
+                    <Feather name="search" size={size} color={color} /> 
+                ),
+            }}
+        />
+        <Tabs.Screen 
+            name="notifications" 
+            options={{
+                title: "Notifications", 
+                tabBarIcon: ({size,color}) => (
+                    <Feather name="heart" size={size} color={color} /> 
+                ),
+            }}
+        />
+        <Tabs.Screen 
+            name="profile" 
+            options={{
+                title: "Profile", 
+                tabBarIcon: ({size,color}) => (
+                    <Feather name="user" size={size} color={color} /> 
+                ),
+            }}
+        />
+        </Tabs>
+    );
+}
