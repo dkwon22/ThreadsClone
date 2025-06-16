1483262e2b65f2729590c95c280532cd1577ad97
David <dkwon22@gmail.com>
2025-06-16 14:38:38 -0400

    path aliases

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,11 +1,12 @@
 
 import { StyleSheet, Text, View } from 'react-native';
+import Button from '@/components/button';
 
 export default function HomeScreen() {
 // text-2xl is the size of the text, font-bold is the font weight, text-red-500 is the color of the text
   return (
-    <View style={styles.container}>
-      <Text className ='text-2xl font-bold text-red-500 p-2'>Feed</Text> 
+    <View className='flex-1 p-4'>
+      <Text className ='text-2xl font-bold text-white p-2'>Hello World</Text> 
     </View>
   );
 }
