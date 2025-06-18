b5fe0a75036458d64ea27569ae402386c723c0d7
David <dkwon22@gmail.com>
2025-06-18 12:57:02 -0400

    tanstack usemutation

--- src/app/(protected)/new.tsx
+++ src/app/(protected)/new.tsx
@@ -3,25 +3,51 @@ import { View, Text, TextInput, Pressable, KeyboardAvoidingView, Platform } from
 import { SafeAreaView } from "react-native-safe-area-context";
 import { supabase } from "@/lib/supabase";
 import { useAuth } from "@/providers/AuthProvider";
+import {useMutation} from '@tanstack/react-query';
+import { router } from "expo-router";
+import { Alert } from "react-native";
+import { useQueryClient } from "@tanstack/react-query";
+
+const createPost = async (content: string, user_id: string) => {
+    const { data } = await supabase.from('posts').insert({
+        content,
+        user_id,
+    }).select('*').throwOnError();
+    return data;
+};
 
 export default function NewPostScreen() {
     const [text, setText] = useState('');
     const { user } = useAuth();
+    const queryClient = useQueryClient();
 
-    const onSubmit = async () => {
-        if (!text || !user) return;
-        const { data, error } = await supabase.from('posts').insert({
-            content: text,
-            user_id: user.id,
-            });
 
-        if (error) {
-            console.error('Error creating post:', error);
-            Alert.alert('Error', error.message);
-            return;
-        }
-        setText('');
-    }
+    const { mutate, isPending, error } = useMutation({
+        mutationFn: () => createPost(text, user!.id),
+        onSuccess: (data) => {
+            setText('');
+            router.back();
+            queryClient.invalidateQueries({queryKey: ['posts']})
+        },
+        onError: (error) => {
+            //Alert.alert('Error', error.message);
+        },
+    }); 
+
+    // const onSubmit = async () => {
+    //     if (!text || !user) return;
+    //     const { data, error } = await supabase.from('posts').insert({
+    //         content: text,
+    //         user_id: user.id,
+    //         });
+
+    //     if (error) {
+    //         console.error('Error creating post:', error);
+    //         Alert.alert('Error', error.message);
+    //         return;
+    //     }
+    //     setText('');
+    // }
     return (
         <SafeAreaView edges={['bottom']} className="p-4 flex-1">
             <KeyboardAvoidingView 
@@ -41,11 +67,15 @@ export default function NewPostScreen() {
                 numberOfLines={4}
                 />
                 
+                {error && <Text className="text-red-500">{error.message}</Text>}
 
                 <View className="mt-auto">
                     <Pressable 
-                    onPress={onSubmit}
-                        className = 'bg-white p-3 px-6 self-end rounded-full'>
+                    onPress={() => mutate()}
+                    disabled={isPending}
+                    className = {`${
+                        isPending ?  ' bg-white/50' : 'bg-white'} p-3 px-6 self-end rounded-full`}
+                    >
                         <Text className="text-black font-bold">Post</Text>
                     </Pressable>
                 </View>
